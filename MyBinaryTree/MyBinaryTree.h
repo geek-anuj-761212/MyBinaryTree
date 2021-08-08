154566efe8a93259e0b086ca29e8552c9df64def
geek-anuj-761212 <87935457+geek-anuj-761212@users.noreply.github.com>
2021-08-08 21:57:02 +0530

    Update MyBinaryTree.h

--- MyBinaryTree/MyBinaryTree.h
+++ MyBinaryTree/MyBinaryTree.h
@@ -11,9 +11,9 @@ using namespace std;
 class TreeNode
 {
 public:
-	TreeNode();          //Constructor 
+	TreeNode();              //Constructor 
 	TreeNode(int data);	 //Constructor with an integer argument
-	TreeNode(int& DATA); //Constructor with an integer value passed by reference
+	TreeNode(int& DATA);     //Constructor with an integer value passed by reference
 	int data;
 	TreeNode* left;
 	TreeNode* right;
@@ -28,19 +28,19 @@ class BinaryTree
 {
 public:
 	TreeNode* getRoot();
-	BinaryTree();			//Constructor
-	~BinaryTree();			//Destructor
+	BinaryTree();		//Constructor
+	~BinaryTree();		//Destructor
 	void insert(int DATA);  //insert function with parameter passed by value
 	void destroy_tree();
 	
 	void In_order();    //////////////////////////////
-	void Pre_order();	///Tree Traversal functions///
+	void Pre_order();   ///Tree Traversal functions///
 	void Post_order();  //////////////////////////////
 private:
 	TreeNode* root;
 	void insert(TreeNode* leaf, int DATA);
 	void destroy_tree(TreeNode* leaf);
-	void In_order(TreeNode* leaf);   ////////////////////////////////////
-	void Pre_order(TreeNode* leaf);  //Private Methods to implement the//
-	void Post_order(TreeNode* leaf); //public traversal methods//////////
+	void In_order(TreeNode* leaf);       ////////////////////////////////////
+	void Pre_order(TreeNode* leaf);      //Private Methods to implement the//
+	void Post_order(TreeNode* leaf);     //public traversal methods//////////
 };
